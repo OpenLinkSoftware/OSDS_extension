5459bdcbb78f9d02896e83d9d38a9dc4d49563a3
Sergey Malinin <smalinin@openlinksw.com>
2023-01-04 18:15:18 +0300

    Updated ChatGPT UI

--- prepare_safari.sh
+++ prepare_safari.sh
@@ -16,19 +16,19 @@ SRC_DIR=./src
 
 #copy common files                                       
 for I_DIR in background.html background.js chat_page.js chat_page.html content.css converters.js frame.js datablock.js; do
-  cp -va $SRC_DIR/$I_DIR $DST_DIR/
+  cp -va $SRC_DIR/$I_DIR "$DST_DIR/"
 done
 
 for I_DIR in dataview.css handlers.js helpers.js helpers_ui.js html_gen.js oidc-webid-inject.js OidcWebid.js options.html options.js; do
-  cp -va $SRC_DIR/$I_DIR $DST_DIR/
+  cp -va $SRC_DIR/$I_DIR "$DST_DIR/"
 done
 
 for I_DIR in page_panel.html page_panel.js panel.html panel.js settings.js sniffer.js tabs.css; do
-  cp -va $SRC_DIR/$I_DIR $DST_DIR/
+  cp -va $SRC_DIR/$I_DIR "$DST_DIR/"
 done
 
 for I_DIR in ttl_gen.js utils.js utils_chat.js ; do
-  cp -va $SRC_DIR/$I_DIR $DST_DIR/
+  cp -va $SRC_DIR/$I_DIR "$DST_DIR/"
 done
 
 
